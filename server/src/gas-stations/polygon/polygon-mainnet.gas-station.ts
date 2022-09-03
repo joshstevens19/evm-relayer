15f5729db4f838a8772ed669570115fc2ff42619
Josh Stevens <joshstevens19@hotmail.co.uk>
2022-09-03 22:40:31 +0100

    fix: remove unused interface

--- server/src/gas-stations/polygon/polygon-mainnet.gas-station.ts
+++ server/src/gas-stations/polygon/polygon-mainnet.gas-station.ts
@@ -1,14 +1,5 @@
 import { BasePolygonGasStation } from './base-polygon.gas-station';
 
-interface PolygonGasStationResult<T> {
-  safeLow: T;
-  standard: T;
-  fast: T;
-  fastest: T;
-  blockTime: T;
-  blockNumber: T;
-}
-
 export class PolygonMainnetGasStation extends BasePolygonGasStation {
   constructor() {
     super('https://gasstation-mainnet.matic.network');
